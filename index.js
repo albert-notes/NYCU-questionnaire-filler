b42989a29d92efbd86e46f34a9ebb3d155dea4e3
Albert Huang <69894231+albert-notes@users.noreply.github.com>
2024-12-27 00:50:09 +0800

    Fix some bugs for filling questions

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function sleep(ms) {
+function sleep(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function fill_form() {
+async function fill_form(){
     var buttons = document.getElementsByClassName('button is-info is-small');
     console.log(buttons);
     while(buttons.length != 0){
@@ -10,29 +10,29 @@ async function fill_form() {
             button.click();
             await sleep(2000);
     
-            for (let i = 0; i <= 10; i++) {
+            for(let i = 0; i <= 15; i++){
                 let n = 'option_1_1_' + String(i);
                 console.log(n);
                 let radio = document.getElementsByName(n);
                 console.log(radio);
-                if (radio.length != 0) {
+                if(radio.length != 0){
                     let ans = Math.round(Math.random());
                     radio[ans].click();
                 }
             }
     
-            for (let i = 0; i <= 10; i++) {
+            for (let i = 0; i <= 15; i++){
                 let n = 'option_2_1_' + String(i);
                 console.log(n);
                 let radio = document.getElementsByName(n);
                 console.log(radio);
-                if (radio.length != 0) {
+                if (radio.length != 0){
                     let ans = Math.round(Math.random());
                     radio[ans].click();
                 }
             }
     
-            while (!document.querySelector('button[data-v-7fe871a7]')) {
+            while(!document.querySelector('button[data-v-7fe871a7]')){
                 await sleep(100);
             }
             let send_button = document.querySelector('button[data-v-7fe871a7]');
